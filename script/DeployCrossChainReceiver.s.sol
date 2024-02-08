57a456ef6002105c11fbefd95c96ff9c0b4b09b2
palmcivet7 <palmcivet@proton.me>
2024-02-08 00:14:46 +0000

    update swapTestnetUsdc deployment

--- script/DeployCrossChainReceiver.s.sol
+++ script/DeployCrossChainReceiver.s.sol
@@ -10,10 +10,10 @@ contract DeployCrossChainReceiver is Script {
 
         address ccipRouterAddressPolygonMumbai = 0x1035CabC275068e0F4b745A29CEDf38E13aF41b1;
         address cometAddressPolygonMumbai = 0xF09F0369aB0a875254fB565E52226c88f10Bc839;
-        address fauceteer = address(0); // deploy SwapTestnetUSDC first
+        address swapTestnetUsdc = 0x77e2D866f2ae9784D260D8ab6Cf36237EB3EA5D2;
 
         CrossChainReceiver crossChainReceiver =
-            new CrossChainReceiver(ccipRouterAddressPolygonMumbai, cometAddressPolygonMumbai, fauceteer);
+            new CrossChainReceiver(ccipRouterAddressPolygonMumbai, cometAddressPolygonMumbai, swapTestnetUsdc);
 
         console.log("CrossChainReceiver deployed to ", address(crossChainReceiver));
 
